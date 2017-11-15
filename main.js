52afaf4da2e374399800123eecb30fd78d8aae7f
Pear <mpearcey.59024@farnborough.ac.uk>
2017-11-15 16:08:02 +0000

    added

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
 function loadPosts() {
 
 $.ajax({
-    url: "https://michaelp59024.github.io/posts/",
-    success: function(data) {
-        var parser = new DOMParser(),
-            doc = parser.parseFromString(data, 'text/html');
-
-        //output the file table
-        $("#files").append(doc.querySelector('table').outerHTML);
-
-        //or return the number of files
-        //tr = icon, filename, date, size, desc
-        //consider all rows with a size value holding a number as a vlid file
-        var fileCount = 0,
-            rows = doc.querySelector('table').querySelectorAll('tr');
-
-        for (var i=0;i<rows.length;i++) {
-            if (rows[i].children[3]) {
-                if (parseInt(rows[i].children[3].innerText)>0) fileCount++;         
-            }
-        }
-        $("#fileCount").text(fileCount+' files');
+    url: "/posts/",
+    success: function (data) {
+        var image_count = $(data).length();     
     }
 });
 }
